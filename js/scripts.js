7f180316e03a4e3fa4584da3dd135e045e08aa39
Francisco R. M. Porfírio <80773101+f-rxfxel@users.noreply.github.com>
2022-09-25 10:50:44 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// RETORNA P/ UNIVÁS
+function bckfnsh() {
+    window.location.href = "https://www.univas.edu.br/";
+}
+
+// NAVEGAÇÃO - SEUS DADOS
+    // VOLTAR
+    /*function bckfnsh() {
+    }*/
+
+    // AVANÇAR
+    function nxtpg1() {
+        window.location.href = "consentimento.html";
+    }
+
+// NAVEGAÇÃO - CONSENTIMENTO
+    // VOLTAR
+    function bckpg2() {
+        window.location.href = "seus-dados.html";
+    }
+
+    // AVANÇAR
+    function nxtpg2() {
+        window.location.href = "confidencialidade.html";
+    }
+
+// NAVEGAÇÃO - CONFIDENCIALIDADE
+    // VOLTAR
+    function bckpg3() {
+        window.location.href = "consentimento.html"
+    }
+
+    // AVANÇAR
+    function nxtpg3() {
+        window.location.href = "termos-assinados.html"
+    }
+
+// NAVEGAÇÃO - TERMOS ASSINADOS
+    /*function bckfnsh() {
+    }*/
